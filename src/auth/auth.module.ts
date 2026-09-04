9b1a9182642f6f738a41f477f2a93083d8d444ce
Mohammed Aashik <aashikdevelop@gmail.com>
2026-09-04 11:49:36 +0400

    Integrate PassportModule and JwtStrategy into AuthModule for improved authentication handling.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,10 +4,13 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtStrategy } from './jwt.strategy';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
   imports: [
     ConfigModule,
+    PassportModule,
 
     JwtModule.registerAsync({
       inject: [ConfigService],
@@ -23,6 +26,6 @@ import { AuthController } from './auth.controller';
   ],
 
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
